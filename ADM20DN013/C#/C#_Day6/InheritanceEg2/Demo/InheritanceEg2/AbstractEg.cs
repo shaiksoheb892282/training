b57d2d49d01d6dbf8acd8c92874383ba8b1b5332
shaiksoheb892282 <77379784+shaiksoheb892282@users.noreply.github.com>
2021-02-01 01:39:20 +0530

    Add files via upload

---
+++ ADM20DN013/C#/C#_Day6/InheritanceEg2/Demo/InheritanceEg2/AbstractEg.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceEg2
+{
+    abstract class RBI
+    {
+        public abstract int HomeLoan();
+        // public abstract void JobSeeker();
+
+        internal string NewAnnouncement()
+        {
+            return "I am a non abstract method of abstract class";
+
+        }
+
+    }
+
+    class Indian : RBI
+    {
+        public override int HomeLoan()
+        {
+            return 8;
+        }
+        // public abstract  override void JobSeeker();
+
+    }
+
+    class HDFC : RBI
+    {
+        public override int HomeLoan()
+        {
+            return 9;
+        }
+
+    }
+    class AbstractEg
+    {
+        static void Main()
+        {
+
+            RBI robj;//Declare one abstract base class variable  
+
+
+            //if choice is 1 then instatiate it with IndianBank child class & go for Homeloan with India Bank;
+            robj= new Indian();
+
+            Console.WriteLine(  robj.NewAnnouncement());//RBI
+            Console.WriteLine(   robj.HomeLoan());//Indian bank
+
+
+            robj = new HDFC();
+            Console.WriteLine(robj.NewAnnouncement());//RBI
+            Console.WriteLine(robj.HomeLoan());//HDFC 
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ADM20DN013/C#/C#_Day6/InheritanceEg2/Demo/InheritanceEg2/AnimalEg.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceEgInternal
+{
+  public class AnimalEg
+    {
+        internal protected string AnimalName()
+        {
+            return "LION";
+        }
+        protected void Welcome()
+        {
+            Console.WriteLine("Welcome  to Protected");
+        }
+        internal void Hello()
+        {
+            Console.WriteLine("Hello is accesible within the same application");
+        }
+    }
+}
+
+
