1f5f3643f86d335d24586ec8c1ccfec3614710c6
shaiksoheb892282 <77379784+shaiksoheb892282@users.noreply.github.com>
2021-02-01 01:48:07 +0530

    Add files via upload

---
+++ ADM20DN013/C#/C#_Day6/InheritanceEg2/Trial/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InheritanceEgInternal;
+
+namespace Trial
+{
+    class Program:AnimalEg
+    {
+        static void Main(string[] args)
+        {
+            Program pobj = new Program();
+           Console.WriteLine( pobj.AnimalName());
+          //  pobj.Hello();It cannot access Hello method as it has internal scope only
+            Console.Read();
+        }
+    }
+}
