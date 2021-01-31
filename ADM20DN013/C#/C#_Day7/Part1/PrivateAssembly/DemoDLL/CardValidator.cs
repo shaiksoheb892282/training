9ce953311c1f40aabd5d22698aef782659f8a72b
shaiksoheb892282 <77379784+shaiksoheb892282@users.noreply.github.com>
2021-02-01 02:19:52 +0530

    Add files via upload

---
+++ ADM20DN013/C#/C#_Day7/Part1/PrivateAssembly/DemoDLL/CardValidator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoDLL
+{
+    public class CardValidator
+    {
+        public bool Validate(string cardno)
+        {
+            string cno= cardno.Trim();
+                if (cno.Length == 16)
+            {
+                //Write the logic here for validating the card no
+              
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
