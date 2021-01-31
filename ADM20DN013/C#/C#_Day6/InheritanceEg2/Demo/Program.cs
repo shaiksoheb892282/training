8e9fb76d25b180b5f0d113ae1e7ac420b193c6b2
shaiksoheb892282 <77379784+shaiksoheb892282@users.noreply.github.com>
2021-02-01 01:33:18 +0530

    Add files via upload

---
+++ ADM20DN013/C#/C#_Day6/InheritanceEg2/Demo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InheritanceEg2;
+
+
+namespace Demo
+{
+    class Program: AnimalEg
+    {
+        static void Main(string[] args)
+        {
+            Program pobj = new Program();
+            Console.WriteLine(pobj.AnimalName());
+            Console.Read();
+        }
+    }
+}
