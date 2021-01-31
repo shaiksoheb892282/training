628ba1e6865f6d8ed793d27d08eaed01c64d07d4
shaiksoheb892282 <77379784+shaiksoheb892282@users.noreply.github.com>
2021-02-01 02:21:08 +0530

    Add files via upload

---
+++ ADM20DN013/C#/C#_Day7/Part1/PrivateAssembly/PrivateAssembly/CalculatorService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrivateAssembly
+{
+    public class CalculatorService
+    {
+        public int Add(int val1,int val2)
+        {
+            return val1 + val2;
+        }
+
+        public int Sub(int val1, int val2)
+        {
+            return val1 - val2;
+        }
+    }
+}
