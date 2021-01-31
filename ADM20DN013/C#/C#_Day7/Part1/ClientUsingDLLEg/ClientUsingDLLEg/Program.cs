115fac8876f06b916a04bb8917c42588333cc3a4
shaiksoheb892282 <77379784+shaiksoheb892282@users.noreply.github.com>
2021-02-01 02:13:27 +0530

    Add files via upload

---
+++ ADM20DN013/C#/C#_Day7/Part1/ClientUsingDLLEg/ClientUsingDLLEg/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoDLL;
+using PrivateAssembly;
+using SharedAssembly;
+namespace ClientUsingDLLEg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //CardValidator obj1 = new CardValidator();
+            //CalculatorService cs = new CalculatorService();
+            //Console.WriteLine(cs.Add(45, 67));
+
+            TaxCalculator tc = new TaxCalculator();
+            tc.Calculator(50000);
+           // Console.WriteLine(  "The Tax Amount if {0}", tc.Calculator(50000));
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
