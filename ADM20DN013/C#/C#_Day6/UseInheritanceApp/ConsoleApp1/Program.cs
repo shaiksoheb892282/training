70ad26569ae986c9078468578c71049795f297f3
shaiksoheb892282 <77379784+shaiksoheb892282@users.noreply.github.com>
2021-02-01 01:55:04 +0530

    Add files via upload

---
+++ ADM20DN013/C#/C#_Day6/UseInheritanceApp/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProtectedInternalEg;
+namespace ConsoleApp1
+{
+    class Program:AnimalEg
+    {
+        internal void WelcomeNew()
+        {
+            Console.WriteLine("Welcome New");
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            
+            p.AnimalName();
+            
+            p.WelcomeNew();
+           
+        }
+    }
+}
