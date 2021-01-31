a581c21e87e12bd3c232c7a3c820d18ec53f094a
shaiksoheb892282 <77379784+shaiksoheb892282@users.noreply.github.com>
2021-02-01 01:19:33 +0530

    Add files via upload

---
+++ ADM20DN013/C#/C#_Day4/PropertyEg/PropertyEg/Employee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyEg
+{
+    class Employee
+    {
+        private int eno;//Private member variable
+
+        public Employee()
+        {
+            eno = 100;
+        }
+
+        public int  GetEmpNo()
+        {
+            return eno;
+        }
+        public int EmployeeNo //property definition
+        {
+            get { return eno; } //accessor
+        //  set { eno = value; } //Mutator
+
+
+            set
+            {
+                if (value > 100 )
+                {
+                    eno = 0;
+                    Console.WriteLine("Employee No should be in the range 0-100 only");
+                }
+                else
+                {
+                    eno = value;
+                }
+            }//Mutator
+        }
+    }
+}
