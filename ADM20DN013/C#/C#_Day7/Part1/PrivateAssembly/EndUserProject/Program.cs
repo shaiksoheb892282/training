de39a30a7f41bb00c198067c98e9dbdf644a2c23
shaiksoheb892282 <77379784+shaiksoheb892282@users.noreply.github.com>
2021-02-01 02:22:05 +0530

    Add files via upload

---
+++ ADM20DN013/C#/C#_Day7/Part1/PrivateAssembly/EndUserProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using PrivateAssembly;
+
+
+namespace EndUserProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculatorService calculatorService = new CalculatorService();
+            Console.WriteLine("Addition:{0}", calculatorService.Add(7, 8));
+            Console.WriteLine("Subtraction:{0}", calculatorService.Sub(20, 8));
+            Console.Read();
+        }
+    }
+}
