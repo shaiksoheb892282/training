90fe6f2bc3023eff3e9b9f4809aed499e0fde771
shaiksoheb892282 <77379784+shaiksoheb892282@users.noreply.github.com>
2021-02-01 01:21:12 +0530

    Add files via upload

---
+++ ADM20DN013/C#/C#_Day4/PropertyEg/PropertyEg/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyEg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee empobj;//Object Declaration
+            
+            empobj= new Employee();//Initialising the object
+
+            Console.WriteLine("After object is initialized employee no is: {0}",empobj.EmployeeNo); //get property Accesor-Reading
+
+            // empobj.eno = 71;
+            empobj.EmployeeNo = 71;//set property Mutator-for writing
+            Console.WriteLine("After object call the set Mutator employee no is: {0}",empobj.EmployeeNo); //get property Accesor
+
+            empobj.EmployeeNo = 121;//set property Mutator-for writing
+            Console.WriteLine("After object call the set Mutator employee no is: {0}", empobj.EmployeeNo);
+            Console.Read();
+
+        }
+    }
+}
