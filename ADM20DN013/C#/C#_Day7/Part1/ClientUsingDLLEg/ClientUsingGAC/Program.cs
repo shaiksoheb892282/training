a12a7039f3007b29c85423ebb105ee9025512c5a
shaiksoheb892282 <77379784+shaiksoheb892282@users.noreply.github.com>
2021-02-01 02:16:34 +0530

    Add files via upload

---
+++ ADM20DN013/C#/C#_Day7/Part1/ClientUsingDLLEg/ClientUsingGAC/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyTaxCalculator;
+namespace ClientUsingGAC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaxCalculator tc = new TaxCalculator();
+            tc.Calculator(40000);
+            Console.ReadKey();
+        }
+    }
+}
