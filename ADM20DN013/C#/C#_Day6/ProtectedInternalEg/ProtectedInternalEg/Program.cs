f166b241c3842a137e04b7bcc54d15ec0df0f188
shaiksoheb892282 <77379784+shaiksoheb892282@users.noreply.github.com>
2021-02-01 00:24:58 +0530

    Add files via upload

---
+++ ADM20DN013/C#/C#_Day6/ProtectedInternalEg/ProtectedInternalEg/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtectedInternalEg
+{
+    public class AnimalEg
+    {
+      protected internal string AnimalName()
+        {
+            return "LION";
+        }
+        protected void Welcome()
+        {
+            Console.WriteLine("Welcome  to Protected");
+        }
+        internal void Hello()
+        {
+            Console.WriteLine("Hello is accesible within the same application");
+        }
+    }
+    class Program
+    {
+        static void  Main()
+        {
+            AnimalEg a = new AnimalEg();
+            a.Hello();
+            a.AnimalName();
+            //a.Welcome();
+        }
+    }
+}
