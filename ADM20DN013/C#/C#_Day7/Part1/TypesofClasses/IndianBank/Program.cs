b88bee1f158eabd9470a0c821162bf294548dcbb
shaiksoheb892282 <77379784+shaiksoheb892282@users.noreply.github.com>
2021-02-01 02:10:02 +0530

    Add files via upload

---
+++ ADM20DN013/C#/C#_Day7/Part1/TypesofClasses/IndianBank/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypesofClasses
+{
+    partial class Mobile
+    {
+        public void TouchPhone()
+        {
+            Console.WriteLine("Touch phone are a boon to senior citizens");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mobile m = new Mobile();
+            m.TouchPhone();
+        }
+    }
+}
