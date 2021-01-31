95f17ef045502550b6d6f37ee5f1934c99eb83ba
shaiksoheb892282 <77379784+shaiksoheb892282@users.noreply.github.com>
2021-02-01 02:08:54 +0530

    Add files via upload

---
+++ ADM20DN013/C#/C#_Day7/Part1/SharedAssembly/MyTaxCalculator/TaxCalculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTaxCalculator
+{
+    public class TaxCalculator
+    {
+
+        public void Calculator(int Salary)
+        {
+            int Result;
+            if ((Salary * 12) > 300000)
+            {
+                Result = (Salary * 12) / 10;
+            }
+            else
+            {
+                Result = 0;
+            }
+
+            Console.WriteLine("Amount to be Paid is :{0}", Result);
+        }
+
+    }
+}
