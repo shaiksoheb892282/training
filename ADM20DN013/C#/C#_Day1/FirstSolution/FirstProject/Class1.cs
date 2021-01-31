cd9610f3c545ea001ad565a8e06d53795427be25
shaiksoheb892282 <77379784+shaiksoheb892282@users.noreply.github.com>
2021-02-01 01:09:56 +0530

    Add files via upload

---
+++ ADM20DN013/C#/C#_Day1/FirstSolution/FirstProject/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    class Class1
+    {
+        static void Method1()
+        {
+            Console.WriteLine("This s method1");
+        }
+        static void Main()
+        {
+            Method1();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ADM20DN013/C#/C#_Day1/FirstSolution/FirstProject/Class2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    class Class2
+    {
+        static void Method2()
+        {
+            Console.WriteLine("Method2 is displayed");
+        }
+        static void Main()
+        {
+            Method2();
+            Console.ReadKey();
+        }
+    }
+}
