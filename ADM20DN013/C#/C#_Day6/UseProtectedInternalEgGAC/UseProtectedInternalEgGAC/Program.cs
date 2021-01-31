791dc5f600aecfc046c2ac4c8efdcf48f2a3cfd9
shaiksoheb892282 <77379784+shaiksoheb892282@users.noreply.github.com>
2021-02-01 01:59:15 +0530

    Add files via upload

---
+++ ADM20DN013/C#/C#_Day6/UseProtectedInternalEgGAC/UseProtectedInternalEgGAC/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProtectedInternalEg;
+namespace UseProtectedInternalEgGAC
+{
+    class Program:AnimalEg
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Console.WriteLine(p.AnimalName());
+            Console.ReadKey();
+        }
+    }
+}
