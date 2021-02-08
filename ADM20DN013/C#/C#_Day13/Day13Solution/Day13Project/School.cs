f6591c1d5c90a7638ca0a3f669ce9a806017dadf
shaiksoheb892282 <77379784+shaiksoheb892282@users.noreply.github.com>
2021-02-08 23:13:05 +0530

    Add files via upload

---
+++ ADM20DN013/C#/C#_Day13/Day13Solution/Day13Project/School.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace Day13Project
+{
+    class School
+    {
+        SqlConnection conn;
+        SqlDataAdapter daCourse;
+        DataSet dsCourse;
+        School()
+        {
+            conn = new SqlConnection(ConfigurationManager.ConnectionStrings["myCon"].ConnectionString);
+        }
+
+        void GetAllCourses()
+        {
+            daCourse = new SqlDataAdapter("proc_GetAllCourse", conn);
+            daCourse.SelectCommand.CommandType = CommandType.StoredProcedure;
+            dsCourse = new DataSet();
+            daCourse.Fill(dsCourse,"myCourse");//open,get the data and put it in dataset,close the connection
+            Console.WriteLine("The connection state "+conn.State);
+            foreach (DataRow row in dsCourse.Tables["myCourse"].Rows)
+                //foreach (DataRow row in dsCourse.Tables[0].Rows)
+            {
+                //Console.WriteLine("Course Id "+row[0]+" name "+row[1]+" fees "+row[2]);
+                Console.WriteLine("Course Id " + row["id"] + " name " + row["name"] + " fees " + row["fees"]);
+            }
+        }
+        void InsertNewCourseToDatabase(string name,float? fees)
+        {
+            InsertNewCourseToDatabase(new Course(name, fees));
+        }
+        void InsertNewCourseToDatabase(string name)
+        {
+            InsertNewCourseToDatabase(new Course(name));
+        }
+        void InsertNewCourseToDatabase(Course course)
+        {           
+            //daCourse = new SqlDataAdapter();
+            //daCourse.InsertCommand = new SqlCommand("proc_InsertCourse", conn);
+            //daCourse.InsertCommand.CommandType = CommandType.StoredProcedure;
+            //daCourse.InsertCommand.Parameters.AddWithValue("@cname", course.Name);
+            //daCourse.InsertCommand.Parameters.AddWithValue("@cfees", course.Fees);
+            //DataSet dataSet = new DataSet();
+            //daCourse.Fill(dataSet);
+            DataRow drNew = dsCourse.Tables[0].NewRow();
+            drNew["name"] = course.Name;
+            drNew["fees"] = course.Fees;
+            dsCourse.Tables["myCourse"].Rows.Add(drNew);
+            new SqlCommandBuilder(daCourse);//generate the insert command
+            daCourse.Update(dsCourse, "myCourse");//Save the values in insert command permanently  in db
+        }
+        static void Main(string[] a)
+        {
+            School school = new School();
+            school.GetAllCourses();
+            Course course = new Course();
+            course.TakeCourseDataFromUser();
+            school.InsertNewCourseToDatabase("HTML");
+            school.GetAllCourses();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ADM20DN013/C#/C#_Day13/Student Management System/Student Management System/SMS.sql
@@ -0,0 +1,57 @@
+use university;
+
+create table Student(ID varchar(10) primary key, Name varchar(30) , DateOfBirth date);
+
+
+select * from Student;
+
+select * from Course;
+
+select * from Enroll;
+
+create table Course(CID varchar(10) primary key, CName varchar(30), CDuration varchar(20), CFees money );
+
+create table Enroll(StudentId varchar(10) references Student(ID),CourseId varchar (10) references Course(CID),DateOfEnroll date, primary key (StudentId,CourseId));
+
+----------------------------------
+
+alter proc procInsertStudent(@stid varchar(10),@stname varchar(30),@stdob date) as
+begin
+insert into Student values(@stid,@stname,@stdob);
+end;
+
+---------------------------------------------
+
+create proc procInsertCourse(@cid varchar(10),@cname varchar(30),@cdur varchar(20), @cfees money) as
+begin
+insert into Course values(@cid,@cname,@cdur,@cfees);
+end;
+
+----------------------------------------------------
+
+create proc procEnroll(@stid varchar(10),@cid varchar(10),@enrolldate date) as
+begin
+insert into Enroll values(@stid,@cid,@enrolldate);
+end;
+
+----------------------------------------------------
+
+alter proc procStudentById(@stid varchar(10)) as
+begin
+select * from Student where ID=@stid;
+end;
+
+-----------------------------------------------------
+
+create proc procCourseById(@cid varchar(10)) as
+begin
+select * from Course where CID=@cid;
+end;
+
+----------------------------------------------------
+
+alter proc procGetEnrollments(@stid varchar(10),@cid varchar(10))as
+begin
+select * from Student where ID=@stid;
+select * from Course where CID=@cid;
+end;
